10c0e8f3b51fbc20d6f379217028c72e1f68f608
quoc1986 <quoc.nguyenhuukhanh@gmail.com>
2023-05-04 16:32:03 +0700

    lesson_01

--- src/lesson_01/Variable.java
+++ src/lesson_01/Variable.java
@@ -1,2 +1,17 @@
-package lesson_01;public class Variable {
+package lesson_01;
+
+public class Variable {
+    public static void main(String[] args) {
+        byte myBytenumber = 1;
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(myBytenumber);
+        System.out.println(Short.MAX_VALUE);
+
+        float myFloatnumber = 2.1f;
+        float myFloatnumber = 3.1f;
+
+
+
+
+    }
 }
